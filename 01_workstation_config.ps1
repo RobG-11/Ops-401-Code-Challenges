5238c0718816c8e60c53350ca693e3b95b354909
user <robert.gregor11@gmail.com>
2023-04-18 15:43:30 +0000

    01v1

---
+++ 01_workstation_config.ps1
@@ -0,0 +1,25 @@
+# Script: 01 - Automating Workstation Configuration
+# Author: Robert Gregor
+# Date of latest revision: 18APR23
+
+# Objectives
+    # Write a PowerShell script that achieves the following objectives
+        # Automatic screen lock
+        # Antivirus installed and scanning
+        # Automatic OS updates enabled
+
+    # Test and validate the script works
+
+# Code Fellows Sources:
+    # [New-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/new-aduser?view=windowsserver2022-ps)
+    
+
+# My Sources:
+    # [Get-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/get-aduser?view=windowsserver2022-ps)
+    # [Format-Table](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.3)
+
+# Main
+
+
+
+# End
