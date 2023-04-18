9161c487dc8081c72ce312ac4afe194b314c221c
user <robert.gregor11@gmail.com>
2023-04-18 15:44:39 +0000

    01v1

--- 01_workstation_config.ps1
+++ 01_workstation_config.ps1
@@ -7,16 +7,10 @@
         # Automatic screen lock
         # Antivirus installed and scanning
         # Automatic OS updates enabled
-
     # Test and validate the script works
 
-# Code Fellows Sources:
-    # [New-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/new-aduser?view=windowsserver2022-ps)
-    
-
 # My Sources:
-    # [Get-ADUser](https://learn.microsoft.com/en-us/powershell/module/activedirectory/get-aduser?view=windowsserver2022-ps)
-    # [Format-Table](https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/format-table?view=powershell-7.3)
+    # 
 
 # Main
 
