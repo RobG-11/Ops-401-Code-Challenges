c62c6760fb95852781b4ae93cd0b23c1d98bd4bc
user <robert.gregor11@gmail.com>
2023-04-19 00:15:54 +0000

    02v1

--- 02_uptime_sensor.py
+++ 02_uptime_sensor.py
@@ -15,18 +15,34 @@
         # Save the output to a text file as a log of events
         # Accept user input for target IP address
 
+# Code Fellows Sources
+    # [How to print current date and time using Python?](https://www.tutorialspoint.com/How-to-print-current-date-and-time-using-Python)
+    # [Python time sleep() Method](https://www.tutorialspoint.com/python/time_sleep.htm)
+    # [Python while Loop Statements](https://www.tutorialspoint.com/python/python_while_loop.htm)
+    # [Python Ping: How to ping with Python](https://www.ictshore.com/python/python-ping-tutorial/)
+    # [Ping 0.2: An implementation of ICMP ping in Python](https://pypi.org/project/ping/)
+
 # My Sources:
     # []()
-    # []()
-    # []()
-    # []()
-    # []()
-    # []()
-    # []()
-    # []()
+    
 
+#!/usr/bin/env python3
 
 # Main
 
+import os, datetime, click
+
+while True:
+    click.clear()
+    print("-------------------------------------")
+    print("Welcome to the uptime sensor program!")
+    print("-------------------------------------\n")
+ 
+    # Requests user_IP input and converts to string
+    user_IP = str(input("Please enter the IP address you would like to ping (or type 'exit'): "))
+
+    if user_IP == "exit":
+        print("\nYou have exited the uptime sensor program successfully!\n")
+        exit()
 
 # End
