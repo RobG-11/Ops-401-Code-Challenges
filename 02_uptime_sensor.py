4124157774fcfb21d5004c614e26d9b1d6d2b83c
user <robert.gregor11@gmail.com>
2023-04-19 01:30:14 +0000

    02v2

--- 02_uptime_sensor.py
+++ 02_uptime_sensor.py
@@ -20,7 +20,10 @@
     # [Python time sleep() Method](https://www.tutorialspoint.com/python/time_sleep.htm)
     # [Python while Loop Statements](https://www.tutorialspoint.com/python/python_while_loop.htm)
     # [Python Ping: How to ping with Python](https://www.ictshore.com/python/python-ping-tutorial/)
-    # [Ping 0.2: An implementation of ICMP ping in Python](https://pypi.org/project/ping/)    
+    # [Ping 0.2: An implementation of ICMP ping in Python](https://pypi.org/project/ping/)
+
+# My Sources
+    # []()
 
 #!/usr/bin/env python3
 
@@ -34,7 +37,7 @@ from pythonping import ping
 def ping_userIP(user_IP):
 
     # For loop used to ping user supplied IP four times
-    for _ in range(4):
+    for i in range(4):
 
         # Declares ping_result variable equal to output of ping command
         ping_result = ping(user_IP, count=1, timeout=1)
