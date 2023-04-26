276da6fd4c7ac45a8960b0d73e542e530d0b0cf3
user <robert.gregor11@gmail.com>
2023-04-26 14:27:36 +0000

    x

---
+++ lab07_stretch.ps1
@@ -0,0 +1,22 @@
+# Script: Lab 07 Stretch Goal - FDE via Powershell
+# Author: Robert Gregor
+# Date of latest revision: 26 APR 23
+
+# Objectives
+    # Write and test a PowerShell script that enables BitLocker and initializes the same FDE process performed through the GUI
+        # 
+
+# My Sources:
+    # [BitLocker: Use BitLocker Drive Encryption Tools to manage BitLocker](https://learn.microsoft.com/en-us/windows/security/information-protection/bitlocker/bitlocker-use-bitlocker-drive-encryption-tools-to-manage-bitlocker)
+    # []()
+
+# Main
+
+# Enables bitlocker for C: drive
+Write-Host "Below are the available drives on your computer"
+Write-Host ""
+Get-PSDrive
+Write-Host ""
+$user_pass = Read-Host -AsSecureString "Please provide a secure password: "
+
+# Enable-BitLockerKeyProtector E: -PasswordProtector -Password $pw
