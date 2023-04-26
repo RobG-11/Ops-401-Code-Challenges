98ef56bf517646dcb92379e06cf270acf6b358a1
user <robert.gregor11@gmail.com>
2023-04-26 15:00:01 +0000

    x

--- lab07_stretch.ps1
+++ lab07_stretch.ps1
@@ -15,14 +15,18 @@
 cls
 Write-Host ""
 Write-Host "Below are the available drives on your computer"
+# Prints all available drives in current PowerShell session
 Get-PSDrive
 Write-Host ""
+# Take user input for drive to encrypt
 $user_drive = Read-Host "Please enter the letter of the drive you would like to encrypt"
+# Adds ":" to drive letter
 $drive_mount_point = $user_drive + ":"
 Write-Host ""
+# Takes user input for bitlocker password
 $user_pass = Read-Host -AsSecureString "Please provide a secure password"
 Write-Host ""
-# Enables bitlocker for user_drive with user_pass
+# Enables bitlocker for user_drive with user_pass , set encryption algorithm, and set full disk encryption
 Enable-BitLocker -MountPoint $drive_mount_point -EncryptionMethod XtsAes256 -UsedSpaceOnly:$false -PasswordProtector -Password $user_pass
 Write-Host "You have successfully enabled bitlocker, your computer will now reboot and encrypt the drive to complete the process"
 Write-Host ""
