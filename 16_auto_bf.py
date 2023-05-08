13ffc8ce9709cc8e112569ce7c92b2c760a1652e
user <robert.gregor11@gmail.com>
2023-05-08 18:58:05 +0000

    16v2

--- 16_auto_bf.py
+++ 16_auto_bf.py
@@ -29,14 +29,36 @@
     # [Iterate over a set in Python](https://www.geeksforgeeks.org/iterate-over-a-set-in-python/)
     # [RockYou Password List](https://github.com/danielmiessler/SecLists/blob/master/Passwords/Leaked-Databases/rockyou.txt.tar.gz)
 
+# My Sources:
+    # [With Open in Python – With Statement Syntax Example](https://www.freecodecamp.org/news/with-open-in-python-with-statement-syntax-example/)
+    # []()
+    # []()
+    # []()
+
 #!/usr/bin/env python
 
 # Import libraries
 import time, getpass
 
-def dict_iterate():
-    exit()
-
+def wordlist_iterate():
+    # Accepts user input path to wordlist
+    word_list = str(input("\nPlease enter the path to your word list (ex. /home/user/wordlist.txt)):\n"))
+    # With statement opens word_list file in read mode as file
+    with open(word_list, 'r') as file:
+        # For loop iterates through each line in word_list
+        for line in file:
+            # Rmoves leading and trailing white space, breaks line into list of words
+            passwords = line.strip().split()
+            # For loop iterates through each password in passwords list
+            for password in passwords:
+                # Assigns current password read to variable
+                variable = password
+                # One second delay
+                time.sleep(1)
+                print()
+                # Prints current password to screen
+                print(variable)
+                
 def recog_passwd():
     exit()
 
@@ -48,11 +70,11 @@ while True:
         print("\n------------------------")
         print("Please choose an option:")
         print("------------------------")
-        print("1) Encrypt file\n2) Decrypt file\n3) Encrypt message string\nexit) Exits program")
+        print("1) List all passwords in word list\n2) Password in word list?\n3) Password complexity evaluation\nexit) Exits program")
         user_option = str(input("------------------------\n"))
 
         if user_option == "1":
-            dict_iterate()
+            wordlist_iterate()
         elif user_option == "2":
             recog_passwd()
         elif user_option == "3":
